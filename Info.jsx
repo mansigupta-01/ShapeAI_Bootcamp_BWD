2cb99020ed135d3fbc677930620f08b869c2c38c
mansigupta-01 <86308940+mansigupta-01@users.noreply.github.com>
2021-06-27 13:32:37 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        A Basic Front end Development with Javascript and React.js Bootcamp
+        taken up by Shaurya Sinha.
+      </p>
+    </div>
+  );
+}
+
+export default Info;
